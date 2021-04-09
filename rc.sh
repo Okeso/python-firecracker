e3cb5a5cc0b067ec80b876c4e086fc2908ba8d41
Hugo Herter <git@hugoherter.com>
2021-04-09 17:03:56 +0200

    Optimize rc.sh

--- rc.sh
+++ rc.sh
@@ -4,17 +4,7 @@ set -euf
 
 echo "=== My Bash RC ==="
 
-#mkdir -p /dev/pts
-#mkdir -p /dev/shm
-
 mount -t proc proc /proc -o nosuid,noexec,nodev
-#mount -t sysfs sys /sys -o nosuid,noexec,nodev
-#mount -t tmpfs run /run -o mode=0755,nosuid,nodev
-##mount -t devtmpfs dev /dev -o mode=0755,nosuid
-#mount -t devpts devpts /dev/pts -o mode=0620,gid=5,nosuid,noexec
-#mount -t tmpfs shm /dev/shm -omode=1777,nosuid,nodev
-
-/root/entropy.py
 
 #echo "Overlay setup"
 mkdir -p /overlay
@@ -35,33 +25,24 @@ ls /dev
 mkdir -p /dev/pts
 mkdir -p /dev/shm
 
-#mount -t proc proc /proc -o nosuid,noexec,nodev
 mount -t sysfs sys /sys -o nosuid,noexec,nodev
 mount -t tmpfs run /run -o mode=0755,nosuid,nodev
 #mount -t devtmpfs dev /dev -o mode=0755,nosuid
 mount -t devpts devpts /dev/pts -o mode=0620,gid=5,nosuid,noexec
 mount -t tmpfs shm /dev/shm -omode=1777,nosuid,nodev
 
-
-sleep 0.5
-sync
-
+# TODO: Move in manager
 ip addr add 172.16.0.2/24 dev eth0
 ip link set eth0 up
 ip route add default via 172.16.0.1 dev eth0
-sync
-
 ip addr
 
 echo "Net up"
 
-/root/entropy.py
+# TODO: Move in manager
+#/usr/sbin/sshd -E /var/log/sshd &
+#
+#echo "SSH UP"
 
-#/usr/sbin/sshd -d -E /var/log/sshd &
-/usr/sbin/sshd -E /var/log/sshd &
-
-echo "SSH UP"
-
-python3 --version
 # Replace this script with the manager
 exec /root/manager.py
